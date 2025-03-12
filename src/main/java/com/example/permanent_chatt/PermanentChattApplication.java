e6919a6d3f83a4b8df5ab731a409cf810863fbf4
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-03-12 14:44:20 +0100

    Initial commit

---
+++ src/main/java/com/example/permanent_chatt/PermanentChattApplication.java
@@ -0,0 +1,13 @@
+package com.example.permanent_chatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermanentChattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermanentChattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/permanent_chatt/PermanentChattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.permanent_chatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermanentChattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
